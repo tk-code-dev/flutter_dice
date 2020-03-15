b4287f9732c1aa4e60dac7cc1e1289349fb42229
tkcode <tkcode.dev@gmail.com>
2020-03-15 10:41:49 -0400

    init commit

---
+++ android/app/src/main/kotlin/com/tkcode/flutter_dice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tkcode.flutter_dice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          title: Text('Dice'),
+          backgroundColor: Colors.indigo,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice2.png'),
+        )
+      ],
+    );
+  }
+}
